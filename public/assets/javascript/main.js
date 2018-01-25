74703fc591da9d0d4cf100e79126382d56e93a36
Pasever <pas1034ever@gmail.com>
2018-01-24 20:25:30 -0500

    a.0.5

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,46 +1,65 @@
-$(document).ready(() => {
-  setTimeout(popup, 3000);
+// $(document).ready(() => {
+//   setTimeout(popup, 3000);
 
-  function popup() {
-    $("#logindiv").css("display", "block");
-  }
+    $("#iflogin").click((e) => {
+      e.preventDefault();
+      popup();
+    });
 
-  $("#login #cancelbtn").click(function() {
-    $(this).parent().parent().hide();
-  });
+    $("#ifsignup").click((e) => {
+      e.preventDefault();
+      signpopup();
+    });
 
-  $("#send").click(function() {
-    let name = $("#name").val();
-    let email = $("#email").val();
-    let contact = $("#contactno").val();
-    let message = $("#message").val();
-    if (name == "" || email == "" || contactno == "" || message == "") {
-      alert("Please Fill All Fields");
-    } else {
-      if (validateEmail(email)) {
-        $("#contactdiv").css("display", "none");
+    function signpopup() {
+      $("#signupdiv").css("display", "block");
+    }
+
+    function popup() {
+      $("#logindiv").css("display", "block");
+    }
+
+    $("#login .cancelbtn").click(function() {
+      $(this).parent().parent().hide();
+    });
+
+    $("#signup .cancelbtn").click(function() {
+      $(this).parent().parent().hide();
+    });
+
+    $("#send").click(function() {
+      let name = $("#name").val();
+      let email = $("#email").val();
+      let contact = $("#contactno").val();
+      let message = $("#message").val();
+      if (name == "" || email == "" || contactno == "" || message == "") {
+        alert("Please Fill All Fields");
       } else {
-        alert('Invalid Email Address');
-      }
-
-      function validateEmail(email) {
-        let filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-        if (filter.test(email)) {
-          return true;
+        if (validateEmail(email)) {
+          $("#contactdiv").css("display", "none");
         } else {
-          return false;
+          alert('Invalid Email Address');
+        }
+
+        function validateEmail(email) {
+          let filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+          if (filter.test(email)) {
+            return true;
+          } else {
+            return false;
+          }
         }
       }
-    }
-  });
+    });
 
-  $("#loginbtn").click(function() {
-    let name = $("#username").val();
-    let password = $("#password").val();
-    if (username == "" || password == "") {
-      alert("Username or Password was Wrong");
-    } else {
-      $("#logindiv").css("display", "none");
-    }
-  });
-});
+    $(".loginbtn").click(function() {
+      let name = $("#username").val();
+      let password = $("#password").val();
+      if (username == "" || password == "") {
+        alert("Username or Password was Wrong");
+      } else {
+        $("#logindiv").css("display", "none");
+      }
+    });
+
+    // });
